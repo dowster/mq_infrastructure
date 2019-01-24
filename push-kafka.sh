cbcce638da99bd29667fa810e94babf7ab8f0961
dowster <brady@dowster.net>
2019-01-23 21:51:47 -0600

    Removed my horrible attempt at multithreading

--- push-kafka.sh
+++ push-kafka.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -e
 
-docker build -t dowster/kafka_server ./kafka_server &
-docker build -t dowster/zookeeper_server ./zookeeper_server &
-wait
+docker build -t dowster/kafka_server ./kafka_server 
+docker build -t dowster/zookeeper_server ./zookeeper_server 
 
-docker tag dowster/kafka_server registry.dowster.net:443/dowster/kafka_server &
-docker tag dowster/zookeeper_server registry.dowster.net:443/dowster/zookeeper_server &
-wait
+docker tag dowster/kafka_server registry.dowster.net:443/dowster/kafka_server 
+docker tag dowster/zookeeper_server registry.dowster.net:443/dowster/zookeeper_server 
 
-docker push registry.dowster.net:443/dowster/kafka_server &
-docker push registry.dowster.net:443/dowster/zookeeper_server &
-wait
+docker push registry.dowster.net:443/dowster/kafka_server 
+docker push registry.dowster.net:443/dowster/zookeeper_server 
