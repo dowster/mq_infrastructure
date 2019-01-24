8a02ec53cc7698b7b4759d4a722f2caefc646b02
dowster <brady@dowster.net>
2019-01-23 21:42:56 -0600

    Scripts to facilitate building and running both kafka and zookeeper servers.

---
+++ push-kafka.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+docker build -t dowster/kafka_server ./kafka_server &
+docker build -t dowster/zookeeper_server ./zookeeper_server &
+wait
+
+docker tag dowster/kafka_server registry.dowster.net:443/dowster/kafka_server &
+docker tag dowster/zookeeper_server registry.dowster.net:443/dowster/zookeeper_server &
+wait
+
+docker push registry.dowster.net:443/dowster/kafka_server &
+docker push registry.dowster.net:443/dowster/zookeeper_server &
+wait
